0546c5a1a687a6e88051edec8a59f0f380f58a24
ClaytonEduard <clayton_eduard@hotmail.com>
2024-03-05 18:30:07 -0300

    Corigindo problemas de navegacao de acordo com a versao router-dom 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import { Home } from './pages/home'
+import { Login } from './pages/login/index'
 
 function App() {
   return (
-    <div className="App">
-      Olá Mundo
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
