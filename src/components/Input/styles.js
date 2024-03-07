6bcc0b8d4a281d91ff897d3eb8222f9c15524758
ClaytonEduard <clayton_eduard@hotmail.com>
2024-03-07 14:16:03 -0300

    Criando tela de Login e add react icons md

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 30px;
+    max-width: 275px;
+    border-bottom: 1px solid #3B3450;
+
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+
+`
+
+export const IconContainer = styled.div`
+    margin-right: 10px;
+`
+
+export const InputText = styled.input`
+    background-color: transparent;
+    color: white;
+    width: 100%;
+    border: 0;
+    height: 30px;
+
+`
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,49 @@
-import { Link } from 'react-router-dom'
-const Login = () => {
-    return (<>
-        <h1>Login</h1>
-        <Link to='/'>
-            Voltar Home
-        </Link>
-    </>
+import { MdEmail, MdLock } from 'react-icons/md'
+
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+
+
+import {
+    Column,
+    Container,
+    CriarText,
+    EsqueciText,
+    Row,
+    SubtitleLogin,
+    Title,
+    TitleLogin,
+    Wrapper
+} from "./styles"
+
+export function Login() {
+    return (
+        <>
+            <Header />
+            <Container>
+                <Column>
+                    <Title>
+                        A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas
+                    </Title>
+                </Column>
+                <Column>
+                    <Wrapper>
+                        <TitleLogin>Faça seu cadastro</TitleLogin>
+                        <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                        <form>
+                            <Input name="email" placeholder="E-mail" leftIcon={<MdEmail />} />
+                            <Input name="password" placeholder="Password" leftIcon={<MdLock />} />
+                            <Button title="Entrar" variant='secundary' />
+                        </form>
+                        <Row>
+                            <EsqueciText>Esqueci minha senha</EsqueciText>
+                            <CriarText>Criar Conta</CriarText>
+                        </Row>
+                    </Wrapper>
+                </Column>
+            </Container>
+        </>
     )
 }
-export { Login }
+
