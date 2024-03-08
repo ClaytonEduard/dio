7528e6543be941f6d72ca2e43be90a1c7dfdeaa6
ClaytonEduard <clayton_eduard@hotmail.com>
2024-03-08 17:42:05 -0300

    validacao de formulario com hook-form

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,6 +4,9 @@ import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 import {
     Column,
@@ -17,9 +20,27 @@ import {
     Wrapper
 } from "./styles"
 
+
+const schema = yup
+    .object({
+        email: yup.string().email('email não é valido').required('Campo obrigatório'),
+        password: yup.string().min(3, "No minimo 3 caracteres").required('Campo obrigatótio'),
+    })
+    .required()
+
 export function Login() {
 
     const navigate = useNavigate();
+
+
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+
+    });
+
+    console.log(isValid, errors);
+    const onSubmit = data => console.log(data);
 
     const handleClickSignIn = () => {
         navigate('/feed')
@@ -39,10 +60,10 @@ export function Login() {
                     <Wrapper>
                         <TitleLogin>Faça seu cadastro</TitleLogin>
                         <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                        <form>
-                            <Input name="email" placeholder="E-mail" leftIcon={<MdEmail />} />
-                            <Input name="password" placeholder="Password" leftIcon={<MdLock />} />
-                            <Button title="Entrar" variant='secundary' onClick={handleClickSignIn} type='button' />
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Input control={control} errorMessage={errors?.email?.message} name="email" placeholder="E-mail" leftIcon={<MdEmail />} />
+                            <Input control={control} errorMessage={errors?.password?.message} name="password" placeholder="Password" type="password" leftIcon={<MdLock />} />
+                            <Button title="Entrar" variant='secundary' type='submit' />
                         </form>
                         <Row>
                             <EsqueciText>Esqueci minha senha</EsqueciText>
@@ -50,7 +71,7 @@ export function Login() {
                         </Row>
                     </Wrapper>
                 </Column>
-            </Container>
+            </Container >
         </>
     )
 }
